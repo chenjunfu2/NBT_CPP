eeb193b4e7f6b1719dec243c811f428bfa26f203
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2025-10-09 01:09:15 +0800

    修改目录结构

---
+++ NBT_CPP.cpp
@@ -0,0 +1,16 @@
+﻿#include "nbt/NBT_All.hpp"
+
+#include <stdio.h>
+
+int main(void)
+{
+	NBT_Node n{};
+
+	NBT_Type::Compound cpd;
+
+	cpd.PutDouble(MU8STR("d"), 0.114514);
+	NBT_Helper::Print(cpd);
+
+
+	return 114514;
+}
