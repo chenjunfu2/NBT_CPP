12adbaa092e011e401bcfec154ff3b28e8beed7b
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2025-10-17 09:44:08 +0800

    1

--- tests/mutf8_test/mutf8_test.cpp
+++ tests/mutf8_test/mutf8_test.cpp
@@ -444,7 +444,7 @@ int mainyy(void)
 
 #include <nbt_cpp/NBT_Node.hpp>
 #include <nbt_cpp/MUTF8_Tool.hpp>
-#include <my/Windows_ANSI.hpp>
+//#include <my/Windows_ANSI.hpp>
 
 #include <stdlib.h>
 #include <locale.h>
