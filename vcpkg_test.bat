06ab2f7d0422eb705779d846231375c7bbd2ee69
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2025-10-20 03:09:23 +0800

    修改测试脚本

--- vcpkg_test.bat
+++ vcpkg_test.bat
@@ -1,4 +1,14 @@
-vcpkg install nbt-cpp[tests] --overlay-ports=.\ports
+vcpkg install nbt-cpp --overlay-ports=.\ports
+pause
+vcpkg remove nbt-cpp
+pause
+
+vcpkg install nbt-cpp[xxhash] --overlay-ports=.\ports
+pause
+vcpkg remove nbt-cpp
+pause
+
+vcpkg install nbt-cpp[gzip] --overlay-ports=.\ports
 pause
 vcpkg remove nbt-cpp
 pause
