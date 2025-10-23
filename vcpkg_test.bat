a0b9872fde93c49e474b932a3c6e29bbf0da844b
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2025-10-24 01:30:47 +0800

    重命名库

--- vcpkg_test.bat
+++ vcpkg_test.bat
@@ -1,19 +1,19 @@
-vcpkg install nbt-cpp --overlay-ports=.\ports
+vcpkg install cjf2-nbt-cpp --overlay-ports=.\ports
 pause
-vcpkg remove nbt-cpp
-pause
-
-vcpkg install nbt-cpp[xxhash] --overlay-ports=.\ports
-pause
-vcpkg remove nbt-cpp
+vcpkg remove cjf2-nbt-cpp
 pause
 
-vcpkg install nbt-cpp[zlib] --overlay-ports=.\ports
+vcpkg install cjf2-nbt-cpp[xxhash] --overlay-ports=.\ports
 pause
-vcpkg remove nbt-cpp
+vcpkg remove cjf2-nbt-cpp
 pause
 
-vcpkg install nbt-cpp[zlib,xxhash] --overlay-ports=.\ports
+vcpkg install cjf2-nbt-cpp[zlib] --overlay-ports=.\ports
 pause
-vcpkg remove nbt-cpp
+vcpkg remove cjf2-nbt-cpp
+pause
+
+vcpkg install cjf2-nbt-cpp[zlib,xxhash] --overlay-ports=.\ports
+pause
+vcpkg remove cjf2-nbt-cpp
 pause
