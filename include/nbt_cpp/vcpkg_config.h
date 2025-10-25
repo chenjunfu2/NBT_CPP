12b033c716d428dad8211ac993c8fdd4745aa670
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2025-10-25 23:36:13 +0800

    修改库名称

--- include/nbt_cpp/vcpkg_config.h
+++ include/nbt_cpp/vcpkg_config.h
@@ -1,3 +1,6 @@
+//示例配置文件，实际安装情况由vcpkg根据可选库生成具体内容
+//示例配置文件内默认包含所有可选库
+
 //use zlib
 #ifndef USE_ZLIB
 #define USE_ZLIB
--- vcpkg_test.bat
+++ vcpkg_test.bat
@@ -1,19 +1,19 @@
-vcpkg install cjf2-nbt-cpp --overlay-ports=.\ports
+vcpkg install chenjunfu2-nbt-cpp --overlay-ports=.\ports
 pause
-vcpkg remove cjf2-nbt-cpp
-pause
-
-vcpkg install cjf2-nbt-cpp[xxhash] --overlay-ports=.\ports
-pause
-vcpkg remove cjf2-nbt-cpp
+vcpkg remove chenjunfu2-nbt-cpp
 pause
 
-vcpkg install cjf2-nbt-cpp[zlib] --overlay-ports=.\ports
+vcpkg install chenjunfu2-nbt-cpp[xxhash] --overlay-ports=.\ports
 pause
-vcpkg remove cjf2-nbt-cpp
+vcpkg remove chenjunfu2-nbt-cpp
 pause
 
-vcpkg install cjf2-nbt-cpp[zlib,xxhash] --overlay-ports=.\ports
+vcpkg install chenjunfu2-nbt-cpp[zlib] --overlay-ports=.\ports
 pause
-vcpkg remove cjf2-nbt-cpp
+vcpkg remove chenjunfu2-nbt-cpp
+pause
+
+vcpkg install chenjunfu2-nbt-cpp[zlib,xxhash] --overlay-ports=.\ports
+pause
+vcpkg remove chenjunfu2-nbt-cpp
 pause
