d0ac4389466be58b5216f9b6e6df2368f3aa7e6d
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2025-10-27 02:27:12 +0800

    修改脚本

--- hash_tag.bat
+++ hash_tag.bat
@@ -1,16 +1,21 @@
 ::vcpkg hash https://github.com/<user>/<repo>/archive/refs/tags/<tag>.tar.gz SHA512
+::vcpkg hash https://github.com/<user>/<repo>/archive/<tag>.tar.gz SHA512
 
 @echo off
 setlocal enabledelayedexpansion
 
 set TAG=%~1
-set URL=https://github.com/chenjunfu2/nbt-cpp/archive/refs/tags/%TAG%.tar.gz
+set URL=https://github.com/chenjunfu2/nbt-cpp/archive/%TAG%.tar.gz
 set FILE=%TEMP%\nbt-cpp-%TAG%.tar.gz
 
 echo Downloading: %URL%
+echo To: %FILE%
+echo.
 curl -L -o "%FILE%" "%URL%" --proxy http://127.0.0.1:7897
 if errorlevel 1 (
     echo Download Error
+    echo.
+    pause
     exit /b 1
 )
 
@@ -21,7 +26,12 @@ vcpkg hash "%FILE%" SHA512
 del "%FILE%"
 if errorlevel 1 (
     echo "%FILE%"
+    echo.
+    pause
     exit /b 1
 )
+
+echo.
+pause
 
 endlocal
