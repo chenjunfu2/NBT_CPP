78e82bed46e38a59c97fd9863a4d5d33034b6cc0
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2025-10-31 08:57:33 +0800

    update

--- include/nbt_cpp/NBT_Array.hpp
+++ include/nbt_cpp/NBT_Array.hpp
@@ -9,7 +9,7 @@ class NBT_Helper;
 /// @brief 一个公开继承自标准库std::vector（在NBT_Type中给出模板实例化）的代理类。
 /// 无特殊成员，构造与使用方式与标准库std::vector一致。
 /// @tparam Array 需要继承的父类类型
-/// @note 用户不应该独立使用使用此类，请使用在NBT_Type中给出的模板实例化类型别名。
+/// @note 用户不应自行实例化此类，请使用NBT_Type::xxxArray来访问此类实例化类型（xxx为具体Array存储的类型）
 template<typename Array>
 class MyArray :public Array//暂时不考虑保护继承
 {
