b99c051dee73dbaebdb17e20b6ad1e1c2272121a
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2025-11-03 23:31:59 +0800

    更新todo和doxy

--- include/nbt_cpp/NBT_Hash.hpp
+++ include/nbt_cpp/NBT_Hash.hpp
@@ -3,6 +3,8 @@
 #include <xxhash.h>
 #include <type_traits>
 
+/// @brief 一个封装xxhash调用的用于计算哈希的辅助类
+/// @note 该类仅在安装xxhash的情况下有效
 class NBT_Hash
 {
 public:
@@ -14,11 +16,14 @@ private:
 	STATE_T *pHashState = nullptr;
 
 public:
+	/// @brief 通过哈希种子构造并初始化xxhash对象
+	/// @param tHashSeed 哈希种子
 	NBT_Hash(const HASH_T &tHashSeed) :pHashState(XXH64_createState())
 	{
 		XXH64_reset(pHashState, tHashSeed);
 	}
 
+	/// @brief 析构并释放xxhash对象，然后置空
 	~NBT_Hash(void)
 	{
 		if (pHashState != nullptr)
@@ -28,13 +33,20 @@ public:
 		}
 	}
 
+	/// @brief 禁止拷贝构造
 	NBT_Hash(const NBT_Hash &_Copy) = delete;
+	/// @brief 移动构造
+	/// @param _Move 右值对象
 	NBT_Hash(NBT_Hash &&_Move) noexcept :pHashState(_Move.pHashState)
 	{
 		_Move.pHashState = nullptr;
 	}
 
+	/// @brief 禁止拷贝赋值
 	NBT_Hash &operator=(const NBT_Hash &) = delete;
+	/// @brief 移动赋值
+	/// @param _Move 右值对象
+	/// @return 对象的引用
 	NBT_Hash &operator=(NBT_Hash &&_Move) noexcept
 	{
 		pHashState = _Move.pHashState;
@@ -42,16 +54,27 @@ public:
 		return *this;
 	}
 
+	/// @brief 根据之前已添加的数据，获取当前的哈希值
+	/// @param 无
+	/// @return 哈希值
 	HASH_T Digest(void)
 	{
 		return XXH64_digest(pHashState);
 	}
 
+	/// @brief 添加指针指向的数据更新哈希
+	/// @param pData 数据指针
+	/// @param szSize 数据大小（字节）
+	/// @note 通过指针访问指定字节数并更新哈希
 	void Update(const void *pData, size_t szSize)
 	{
 		XXH64_update(pHashState, pData, szSize);
 	}
 
+	/// @brief 通过可平凡复制类型更新哈希
+	/// @tparam T 可平凡复制类型
+	/// @param tData 类型的引用
+	/// @note 自动计算类型字节数并更新哈希
 	template<typename T>
 	requires(std::is_trivially_copyable_v<T>)
 	void Update(const T &tData)
@@ -59,6 +82,10 @@ public:
 		Update(&tData, sizeof(tData));
 	}
 
+	/// @brief 通过可平凡复制类型的数组更新哈希
+	/// @tparam T 可平凡复制类型
+	/// @param tDataArr 可平凡复制类型的数组引用
+	/// @note 自动计算数组字节数并更新哈希
 	template<typename T, size_t N>
 	requires(std::is_trivially_copyable_v<T>)
 	void Update(const T(&tDataArr)[N])
@@ -67,11 +94,23 @@ public:
 	}
 
 public:
+	/// @brief 直接通过指针指向的数据获取哈希
+	/// @param pData 数据指针
+	/// @param szSize 数据大小（字节）
+	/// @param tHashSeed 哈希种子
+	/// @return 计算的哈希值
+	/// @note 直接计算并返回，无状态，静态函数
 	static HASH_T Hash(const void *pData, size_t szSize, HASH_T tHashSeed)
 	{
 		return XXH64(pData, szSize, tHashSeed);
 	}
 
+	/// @brief 直接通过可平凡复制类型获取哈希
+	/// @tparam T 可平凡复制类型
+	/// @param tData 可平凡复制类型的引用
+	/// @param tHashSeed 哈希种子
+	/// @return 计算的哈希值
+	/// @note 自动获取类型字节数并直接计算后返回，无状态，静态函数
 	template<typename T>
 	requires(std::is_trivially_copyable_v<T>)
 	static HASH_T Hash(const T &tData, HASH_T tHashSeed)
@@ -79,6 +118,12 @@ public:
 		return Hash(&tData, sizeof(tData), tHashSeed);
 	}
 
+	/// @brief 直接通过可平凡复制类型的数组获取哈希
+	/// @tparam T 可平凡复制类型
+	/// @param tDataArr 可平凡复制类型的数组引用
+	/// @param tHashSeed 哈希种子
+	/// @return 计算的哈希值
+	/// @note 自动获取数组字节数并直接计算后返回，无状态，静态函数
 	template<typename T, size_t N>
 	requires(std::is_trivially_copyable_v<T>)
 	static HASH_T Hash(const T(&tDataArr)[N], HASH_T tHashSeed)
