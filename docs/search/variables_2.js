372d13660031ab5c06a8b0a3d9c8097a17f39627
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-02-24 13:13:19 +0800

    doxygen update

--- docs/search/variables_2.js
+++ docs/search/variables_2.js
@@ -4,8 +4,12 @@ var searchData=
   ['int_5fmin_1',['Int_Min',['../classNBT__Type.html#a0fa60f16ae2ba4a6e16ea767f9cffc9c',1,'NBT_Type']]],
   ['is_5fconst_2',['is_const',['../classNBT__Node__View.html#a178870b77084f0363a67bbd1622d773a',1,'NBT_Node_View']]],
   ['isarraytype_5fv_3',['IsArrayType_V',['../classNBT__Type.html#ab0dace6d74cd4e85f28a52c768f198ec',1,'NBT_Type']]],
-  ['isfloatingtype_5fv_4',['IsFloatingType_V',['../classNBT__Type.html#abba0d08fee2e48ecd59247d760ad6dca',1,'NBT_Type']]],
-  ['isintegertype_5fv_5',['IsIntegerType_V',['../classNBT__Type.html#a4c376aa5e59ded392dc860e3ac745436',1,'NBT_Type']]],
-  ['isnumerictype_5fv_6',['IsNumericType_V',['../classNBT__Type.html#a133b229478ee9047158f6f99900b3d59',1,'NBT_Type']]],
-  ['isvalidtype_5fv_7',['IsValidType_V',['../classNBT__Type.html#a71dfa77059d0a3bd9a6190295ad400ca',1,'NBT_Type']]]
+  ['iscompoundtype_5fv_4',['IsCompoundType_V',['../classNBT__Type.html#a55bc8c31fcc208e2cbed63fd5bdf3a1a',1,'NBT_Type']]],
+  ['iscontainertype_5fv_5',['IsContainerType_V',['../classNBT__Type.html#a5732a67f89efd315f37f20f92a4defbe',1,'NBT_Type']]],
+  ['isfloatingtype_5fv_6',['IsFloatingType_V',['../classNBT__Type.html#abba0d08fee2e48ecd59247d760ad6dca',1,'NBT_Type']]],
+  ['isintegertype_5fv_7',['IsIntegerType_V',['../classNBT__Type.html#a4c376aa5e59ded392dc860e3ac745436',1,'NBT_Type']]],
+  ['islisttype_5fv_8',['IsListType_V',['../classNBT__Type.html#aba1bda392fa0eaa04f27f22ecf1c7446',1,'NBT_Type']]],
+  ['isnumerictype_5fv_9',['IsNumericType_V',['../classNBT__Type.html#a133b229478ee9047158f6f99900b3d59',1,'NBT_Type']]],
+  ['isstringtype_5fv_10',['IsStringType_V',['../classNBT__Type.html#a0c20f7e8573825530c8ba2a75b431b46',1,'NBT_Type']]],
+  ['isvalidtype_5fv_11',['IsValidType_V',['../classNBT__Type.html#a71dfa77059d0a3bd9a6190295ad400ca',1,'NBT_Type']]]
 ];
