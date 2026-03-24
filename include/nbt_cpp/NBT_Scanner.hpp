8e3427f185a1ae5c921cad3e6df32f8af70d78ae
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-03-24 19:17:29 +0800

    1

--- include/nbt_cpp/NBT_Scanner.hpp
+++ include/nbt_cpp/NBT_Scanner.hpp
@@ -5,6 +5,8 @@
 
 class NBT_Scanner
 {
+	template<typename T>
+	using DefaultInputStream = NBT_Reader::DefaultInputStream<T>;
 
 
 
@@ -12,7 +14,21 @@ class NBT_Scanner
 
 
 
+public:
+	template<typename InputStream, typename Visitor>
+	requires(IsLookLike_NBT_Visitor<Visitor>)
+	static void Scan(InputStream IptStream, Visitor &tVisitor)
+	{
+		//TODO
+	}
+	
+	template<typename DataType = std::vector<uint8_t>, typename Visitor>
+	requires(IsLookLike_NBT_Visitor<Visitor>)
+	static void Scan(const DataType &tDataInput, size_t szStartIdx, Visitor &tVisitor)
+	{
+		DefaultInputStream<DataType> IptStream(tDataInput, szStartIdx);
 
 
-
+		//TODO
+	}
 };
